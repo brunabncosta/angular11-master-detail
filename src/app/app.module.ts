c8fcff56c3378e6bd071700c1e257e1fad9ec048
bbeatriz <brunabeatrizncosta@gmail.com>
2021-04-13 20:28:04 -0300

    Melhorando o category list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,7 +15,9 @@ import { InMemoryDatabase } from "./in-memory-database";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/categories/category-list/category-list.component.ts
+++ src/app/pages/categories/category-list/category-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Category } from "../shared/category.model";
+import { CategoryService } from "../shared/category.service";
+
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
@@ -7,14 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
+  categories: Category[] = [];
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
+    this.categoryService.getAll().subscribe(
+      (categories => this.categories = categories),
+      error => alert('Erro ao carregar a lista')
+    )
   }
 
-  //Adicionei para parar erro no categoryListComponent
-  alert() {
+  deleteCategory(category: Category) {
+    const mustDelete = confirm('Deseja realmente excluir este item?');
 
+    if (mustDelete) {
+      this.categoryService.delete(category.id).subscribe(
+      () => this.categories = this.categories.filter(element => element != category),
+      () => alert("Erro ao tentar excluir!")
+      )
+    }
   }
+
 }
